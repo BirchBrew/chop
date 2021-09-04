86740f9023c88ef82fe424d8fbe6398c3190617c
Zak Miller <zak@zakmiller.com>
2021-09-04 14:10:07 -0400

    Better names

--- init.pl
+++ init.pl
@@ -1,8 +1,11 @@
 :- use_module(download).
 :- use_module(config).
 
-download_to_disk(URL, Version) :- format(atom(Formatted), 'deps/~w', [Version]),
-down(URL, Formatted).   
+download_to_disk(URL, Version) :- 
+    split_string(URL, "/", "", Pieces),
+    last(Pieces, PackageName),
+    format(atom(Formatted), 'deps/~w', [PackageName]),
+    down(URL, Formatted).   
 
 :- forall(dependency(URL, Version), download_to_disk(URL, Version)).
   
