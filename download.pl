163e6eff6e10bcd2f8d9d40fac49a0fbf2a2a8e8
Zak Miller <zak@zakmiller.com>
2021-09-05 18:52:54 -0400

    Switch to simpler download implementation

--- download.pl
+++ download.pl
@@ -2,20 +2,14 @@
 
 :- use_module(library(http/http_open)).
 :- set_prolog_flag(double_quotes, chars).
-:- use_module(library(clpfd)).
 
-loop_through_list(File, List) :-
-    member(Element, List),
-    write(File, Element),
-    fail.
-
-write_list_to_file(Filename,List) :-
-    open(Filename, write, File),
-    \+ loop_through_list(File, List),
-    close(File).
-
-down(X, F) :- http_open(X, In, []),
-read_stream_to_codes(In, Codes),
-maplist(char_code, Chars, Codes),
-write_list_to_file(F, Chars),
-close(In).
+down(URL, LocalFile) :-
+    setup_call_cleanup(
+      http_open(URL, In, []),
+      setup_call_cleanup(
+        open(LocalFile, write, Out),
+        copy_stream_data(In, Out),
+        close(Out)
+        ),
+      close(In)
+    ).
